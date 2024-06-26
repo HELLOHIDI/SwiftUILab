060d7fe61a525d13ea69ba5151af3845b92c2569
HELLOHIDI <fbgmlwo123@naver.com>
2024-06-27 01:41:53 +0900

    [Add] Color+ Extension 추가

---
+++ HiLobaNote/HiLobaNote/App/HiLobaNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiLobaNoteApp.swift
+//  HiLobaNote
+//
+//  Created by 류희재 on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct HiLobaNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HiLobaNote/HiLobaNote/Core/Extension/Color+Extension.swift
@@ -0,0 +1,24 @@
+//
+//  Color+Extension.swift
+//  HiLobaNote
+//
+//  Created by 류희재 on 6/27/24.
+//
+
+import Foundation
+
+import SwiftUI
+
+extension Color {
+  static let customBackgroundGreen = Color("customBackgroundGreen")
+  static let customBlack = Color("customBlack")
+  static let customCoolGray = Color("customCoolGray")
+  static let customGray0 = Color("customGray0")
+  static let customGray1 = Color("customGray1")
+  static let customGray2 = Color("customGray2")
+  static let customGreen = Color("customGreen")
+  static let customIconGray = Color("customIconGray")
+  static let customOrange = Color("customOrange")
+  static let customSky = Color("customSky")
+  static let customWhite = Color("customWhite")
+}
