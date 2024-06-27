0a3e6c3fd85f0a295c11ed0c4d0d3c0083b95c48
HELLOHIDI <fbgmlwo123@naver.com>
2024-06-27 15:15:14 +0900

    [Feat] Todo 모델, 뷰모델 만들기 #3

--- HiLobaNote/HiLobaNote/Feature/Todo/TodoViewModel.swift
+++ HiLobaNote/HiLobaNote/Feature/Todo/TodoViewModel.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+class TodoViewModel: ObservableObject {
+    @Published var todos: [Todo]
+    @Published var isEditTodoMode: Bool
+    @Published var removeTodos: [Todo]
+    @Published var isDisplayRemoveTodoAlert: Bool
+    
+    var removeTodosCount: Int {
+        return removeTodos.count
+    }
+    var naigationBarRightBtnMode: NavigationBthType {
+        isEditTodoMode ? .complete : .edit
+    }
+    
+    init(
+         todos: [Todo] = [],
+         isEditTodoMode: Bool = false,
+         removeTodos: [Todo] = [],
+         isDisplayRemoveTodoAlert: Bool = false) {
+        self.todos = todos
+        self.isEditTodoMode = isEditTodoMode
+        self.removeTodos = removeTodos
+        self.isDisplayRemoveTodoAlert = isDisplayRemoveTodoAlert
+    }
+}
--- HiLobaNote/HiLobaNote/Model/Todo/Todo.swift
+++ HiLobaNote/HiLobaNote/Model/Todo/Todo.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+struct Todo: Hashable {
+    var title: String
+    var time: Date
+    var day: Date
+    var selected: Bool
+    
+    var convertedDayAndTime: String {
+        // 오늘 - 오후 03:00에 알림
+        String("\(day.formattedDay) - \(time.formattedTime)에 알림")
+    }
+    
+    init(title: String, time: Date, day: Date, selected: Bool) {
+        self.title = title
+        self.time = time
+        self.day = day
+        self.selected = selected
+    }
+}
