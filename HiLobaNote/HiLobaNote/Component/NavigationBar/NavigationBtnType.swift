0e9c52a248d33a3bab3518a7b8d26c835adcdf88
HELLOHIDI <fbgmlwo123@naver.com>
2024-06-28 23:13:00 +0900

    [Conflict] 깃 컨플릿 해결

---
+++ HiLobaNote/HiLobaNote/Component/NavigationBar/NavigationBtnType.swift
@@ -0,0 +1,15 @@
+//
+//  NavigationBtnType.swift
+//  HiLobaNote
+//
+//  Created by 류희재 on 6/28/24.
+//
+
+import Foundation
+
+enum NavigationBtnType: String {
+  case close
+  case edit = "편집"
+  case complete = "완료"
+  case create = "생성"
+}
