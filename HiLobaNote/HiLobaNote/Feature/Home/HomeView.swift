38dd96be28771ca7ee89cc740d57fac71fa3c08a
HELLOHIDI <fbgmlwo123@naver.com>
2024-07-06 15:54:45 +0900

    [Feat] 홈뷰 구현 #7

--- HiLobaNote/HiLobaNote/Feature/Home/HomeView.swift
+++ HiLobaNote/HiLobaNote/Feature/Home/HomeView.swift
@@ -8,11 +8,102 @@
 import SwiftUI
 
 struct HomeView: View {
+    @EnvironmentObject private var pathModel: PathModel
+    @StateObject private var homeViewModel = HomeViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            //탭뷰
+            TabView(selection: $homeViewModel.selectedTab) {
+                TodoListView()
+                    .tabItem {
+                        Image (
+                            homeViewModel.selectedTab == .todoList
+                            ? "todoIcon_selected"
+                            : "todoIcon"
+                        )
+                    }
+                    .tag(Tab.todoList)
+                
+                TodoListView()
+                    .tabItem {
+                        Image (
+                            homeViewModel.selectedTab == .todoList
+                            ? "todoIcon_selected"
+                            : "todoIcon"
+                        )
+                    }
+                    .tag(Tab.todoList)
+                
+                MemoListView()
+                    .tabItem {
+                        Image (
+                            homeViewModel.selectedTab == .memo
+                            ? "memoIcon_selected"
+                            : "memoIcon"
+                        )
+                    }
+                    .tag(Tab.memo)
+                
+                VoiceRecorderView()
+                    .tabItem {
+                        Image (
+                            homeViewModel.selectedTab == .voiceRecorder
+                            ? "recordIcon_selected"
+                            : "recordIcon"
+                        )
+                    }
+                    .tag(Tab.voiceRecorder)
+                
+                TimerView()
+                    .tabItem {
+                        Image (
+                            homeViewModel.selectedTab == .timer
+                            ? "timerIcon_selected"
+                            : "timerIcon"
+                        )
+                    }
+                    .tag(Tab.voiceRecorder)
+                
+                SettingView()
+                    .tabItem {
+                        Image (
+                            homeViewModel.selectedTab == .setting
+                            ? "settingIcon_selected"
+                            : "settingIcon"
+                        )
+                    }
+                    .tag(Tab.setting)
+            }
+            
+            //구분선
+            SeperatorLineView()
+        }
+    }
+}
+
+private struct SeperatorLineView: View {
+    fileprivate var body: some View {
+        VStack {
+            Spacer()
+            
+            Rectangle()
+                .fill(
+                    LinearGradient(
+                        gradient: Gradient(colors: [Color.white, Color.gray.opacity(0.1)]),
+                        startPoint: .top,
+                        endPoint: .bottom
+                    )
+                )
+                .frame(height: 10)
+                .padding(.bottom, 60)
+        }
     }
 }
 
 #Preview {
     HomeView()
+        .environmentObject(PathModel())
+        .environmentObject(TodoListViewModel())
+        .environmentObject(MemoListViewModel())
 }
---
+++ HiLobaNote/HiLobaNote/Model/Home/Tab.swift
@@ -0,0 +1,16 @@
+//
+//  Tab.swift
+//  HiLobaNote
+//
+//  Created by 류희재 on 7/6/24.
+//
+
+import Foundation
+
+enum Tab {
+    case todoList
+    case memo
+    case voiceRecorder
+    case timer
+    case setting
+}
