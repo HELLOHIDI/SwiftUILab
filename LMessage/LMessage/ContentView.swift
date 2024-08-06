6d89fe8e6fc7735fe069592ac2868cc63af186ea
HELLOHIDI <fbgmlwo123@naver.com>
2024-08-06 23:41:37 +0900

    [Setting] LMessage 세팅

---
+++ LMessage/LMessage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LMessage
+//
+//  Created by 류희재 on 8/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LMessage/LMessage/LMessageApp.swift
@@ -0,0 +1,17 @@
+//
+//  LMessageApp.swift
+//  LMessage
+//
+//  Created by 류희재 on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct LMessageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
